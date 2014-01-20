56559445e6e13d87addb9e1afb3e167dbb928c04
Omar Moreno <omoreno1@ucsc.edu>
2014-01-20 08:18:37 -0800

    Add ability to process both a single file and a list of files; Add some additional histograms; Make threshold settable; Check that all samples above threshold are consecutive

--- src/analysis/source_analysis.cxx
+++ src/analysis/source_analysis.cxx
@@ -43,11 +43,11 @@
 
 //--- Analysis ---//
 //----------------//
-#include "utils/PlotUtils.h"
-#include "utils/Apv25Utils.h"
-#include "utils/SvtCalibrations.h"
-#include "utils/NearestNeighbor.h"
-#include "utils/Cluster.h"
+#include <PlotUtils.h>
+#include <Apv25Utils.h>
+#include <SvtCalibrations.h>
+//#include "utils/NearestNeighbor.h"
+//#include "utils/Cluster.h"
 
 using namespace std;
 using namespace RooFit;  
@@ -56,24 +56,27 @@ void displayUsage();
 
 int main(int argc, char **argv)
 {
-	string input_file_name;
+	string input_file_name = ""; 
+	string input_file_list_name = ""; 
 	string output_file_name = "";
 	string output_path = ".";
 	string baseline_path = "";
 	int number_events = 0;
 	int run = 0;
 	int option_char;
+	int threshold = 2; // Noise RMS 	
 
 	// Parse any command line arguments. If there are no valid command line
 	// arguments given, print the usage.
-	while((option_char = getopt(argc, argv, "o:h:f:un:p:b:r:i:")) != -1){
+	while((option_char = getopt(argc, argv, "h:f:un:p:b:r:i:t:")) != -1){
 		switch(option_char){
 			case 'i':
 				// Name of input file
 				input_file_name = optarg;
-			case 'o':
-				// Name of the output file
-				output_file_name = optarg;
+				break;
+			case 'l': 
+				// Name of the list that contains the files to process
+				input_file_list_name = optarg; 
 				break;
 			case 'n':
 				// The number of events to process
@@ -91,6 +94,10 @@ int main(int argc, char **argv)
 				// The run number
 				run = atoi(optarg);
 				break;
+			case 't':
+				// Threshold in noise RMS
+				threshold = atoi(optarg);
+				break; 	
 			case 'u':
 				displayUsage();
 			default:
@@ -98,13 +105,40 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// Open the input stream
-	ifstream input_file(input_file_name.c_str(), ifstream::in);
+	// If a valid input file name or a valid input file list has not been specified, 
+	// exit the program
+	if(input_file_name.length() == 0 && input_file_list_name.length() == 0){
+		cerr << "\nPlease specify a file name or a list of files."
+	    	 << "\nUse '-h' flag for usage.\n" << endl;
+		return EXIT_FAILURE;
+	} else if(input_file_name.length() > 0 && input_file_list_name.length() > 0){
+		cerr << "\nCannot specify both input file name and a list of files." << endl;
+		return EXIT_FAILURE; 
+	}
 
-	// Check if the file was opened successfully. If not, exit
-	if(!input_file.is_open()){
-		cerr << "Failed to open file " << input_file_name << "!" << endl;
-		return EXIT_FAILURE; 	
+	// If a valid run number has not been specified, exit the program
+	if(run <= 0){ 
+		cerr << "\nPlease specify a valid run number."
+			 << "\nUse '-h' flag for usage.\n" << endl;
+		return EXIT_FAILURE; 
+	}
+
+
+	// Create a list of files to process
+	list<string> files;
+	string file; 
+	if(input_file_name.length() > 0){
+		files.push_back(input_file_name); 
+	} else if(input_file_list_name.length() > 0){
+		ifstream input_file_list(input_file_list_name.c_str(), ifstream::in);
+		// Check if the file was opened successfully. If not, exit
+		if(!input_file_list.is_open()){
+			cerr << "Failed to open file " << input_file_list << "!" << endl;
+			return EXIT_FAILURE; 
+		}
+		while(input_file_list >> file){
+			files.push_back(file); 
+		}
 	}
 
 	// Instantiate the objects needed to read the tracker data
@@ -115,73 +149,88 @@ int main(int argc, char **argv)
 	//--- Setup Histograms ---//
 	//------------------------//
 	gErrorIgnoreLevel = kWarning;
+
+	// Setup the canvas
 	TCanvas *canvas = new TCanvas("canvas", "canvas", 500, 500);
 	PlotUtils::setupCanvas(canvas);
 	PlotUtils::setContours();
 
+	// Fitted signal amplitude for all channels combined
 	TH1F* h_sig_amp = new TH1F("sig_amp", "Fitted Signal Amplitude", 34, 0, 1190);
 	h_sig_amp->SetLineColor(kBlue-4);
 	h_sig_amp->SetLineWidth(2);
 
+	// Fitted signal amplitude by channel
+	vector<TH1F*> sig_amps; 
+	PlotUtils::initMulti1DPlots(640, sig_amps, "Fitted Signal Amplitudes", 34, 0, 1190);
+	
+	// Fitted signal amplitude of single hit clusters for all channels combined
 	TH1F* h_sh_cluster_amp = new TH1F("sh_cluster_amp", "Single Hit Cluster Amplitude", 34, 0, 1190);
 	h_sh_cluster_amp->SetLineColor(kGreen+3);
 	h_sh_cluster_amp->SetLineWidth(2);
 
-	vector<TH1F*> sig_amps; 
-	PlotUtils::initMulti1DPlots(640, sig_amps, "Fitted Signal Amplitudes", 34, 0, 1190);
-
+	// 2D histogram of the fitted signal amplitude vs channel 
 	TH2F* sig_amp_vs_channel = new TH2F("sig_amp_vs_channel", "Fitted Signal Amplitude vs Channel", 640, 0, 640, 34, 0, 1190);
 
+	TGraphErrors* g_mean_sig_amp = new TGraphErrors(); 
+
+	// The samples for those events which pass the signal cuts
 	vector<TH2F*> signal_samples;
 	PlotUtils::initMulti2DPlots(640, signal_samples, "Signal_Samples", 6, 0, 150, 400, -100, 700);
 
+	// The number of events per channel
 	TGraph* g_n_events = new TGraph();
-	TGraphErrors* g_gain_stability = new TGraphErrors(); 	
 
+	//
 	TH1F* h_n_hits = new TH1F("n_hits", "Number of Hits Above Threshold", 10, 0, 10);
 	h_n_hits->SetLineColor(kBlue-4); 
 	h_n_hits->SetLineWidth(2); 	
 
+	//
 	TH1F* h_n_clusters = new TH1F("n_clusters", "Number of Clusters", 10, 0, 10);
 	h_n_clusters->SetLineColor(kBlue-4); 
 	h_n_clusters->SetLineWidth(2); 	
 
+	//
 	TH1F* h_n_hits_clusters = new TH1F("n_hit_clusters", "Number of Hits per Clusters", 10, 0, 10);
 	h_n_clusters->SetLineColor(kBlue-4); 
 	h_n_clusters->SetLineWidth(2); 	
 
-	// Load half-module calibrations.  If they can't be found or aren't loaded,
-	// exit.
-	if(!SvtCalibrations::getInstance()->loadCalibrations(baseline_path)){
-		cout << "Unable to find calibrations! Exiting ..." << endl;
-		return EXIT_FAILURE;
-	};
+	// Plots of all samples when the given number of samples are above threshold
+	TH1F* h_one_above_thresh = new TH1F("h_one_above_thresh", "Single Sample Above Threshold", 100, -200, 1190);
+	TH1F* h_two_above_thresh = new TH1F("h_two_above_thresh", "Two Samples Above Threshold", 100, -200, 1190);
+	TH1F* h_three_above_thresh = new TH1F("h_tree_above_thresh", "Three Samples Above Threshold", 100, -200, 1190);
+	TH1F* h_four_above_thresh = new TH1F("h_four_above_thresh", "Four Samples Above Threshold", 100, -200, 1190);
 
-	NearestNeighbor* clusterer = new NearestNeighbor();
+	// Load calibrations
+	bool calibrations_loaded = SvtCalibrations::getInstance()->loadCalibrations(baseline_path); 
+
+	//NearestNeighbor* clusterer = new NearestNeighbor();
 
 	// Output stream used to dump the configuration file
 	ofstream output_config; 
-	string file, file_path, file_name, input_line;
+	string file_path, file_name, input_line;
 	double sig_samples[6] = {0};
 	int channel;
 	double max_sample;
 	int event_number = 0;
 	double baseline, noise;
-	int threshold = 3;
-	int pass_threshold = 0;
+	int pass_threshold[6] = {0};
+	int threshold_sum = 0; 
 	int fpga = 0; 
-	int hybrid = 0; 
+	int hybrid = 1;
+	uint pedestal = 0; 	
 	double fit_params[4] = {0};
 	map<int, double> hit_map;
 	bool read_config = false; 
-	while(input_file >> input_line){
+	for(list<string>::iterator files_it = files.begin(); files_it != files.end(); files_it++){
 
-		// Skip lines which are commented out
-		if( int(input_line.find("#")) == 0 ) continue;
+		cout << "Processing file " << *files_it << endl;
 
-		cout << "Processing file " << input_line << endl;
-
-		if(!dataRead->open(input_line)) return EXIT_FAILURE;
+		if(!dataRead->open(*files_it)){
+			cerr << "Files not found!" << endl;
+			return EXIT_FAILURE;
+		}
 
 		// flag to indicate if configuration should be dumped and values should
 		// be read.  this will be done at the beginning of each calibration run.
@@ -195,6 +244,8 @@ int main(int argc, char **argv)
 
 			if(read_config){ 
 
+				cout << "Reading configuration" << endl;
+
 				// All configuration files will be dumped into a "config"
 				// directory created within the output path specified by the
 				// user.  If the config directory doesn't exist, create it.
@@ -204,12 +255,13 @@ int main(int argc, char **argv)
 					mkdir(file_path.c_str(), 0777);
 				}
 
-				file_name = input_line.substr(input_line.find_last_of("/") + 1);
+				file_name = *files_it; 
+				file_name = file_name.substr(input_line.find_last_of("/") + 1);
 				file_name.erase(file_name.find("."));
 				file_name += ".config";
 				output_config.open((file_path + "/" + file_name).c_str());
 				if(output_config.is_open()){ 
-					dataRead->dumpConfig(output_config); 
+					//dataRead->dumpConfig(output_config); 
 					cout << "DAQ configuration has been dumped to " + file_path + "/" + file_name << endl;
 				}
 				output_config.close(); 
@@ -221,16 +273,12 @@ int main(int argc, char **argv)
 			// Once the number of desired events has been reached, skip the
 			// rest of the events
 			++event_number;
-			if(event_number == number_events){
-				event_number--;
-				break;
-			}
-			if(event_number%10000 == 0){
-			//if(event_number%1 == 0){
+			if(event_number == number_events) break;
+			if(event_number%1000 == 0){
 				cout << "Event: " << event_number << endl;
 			}
 
-			hit_map.clear(); 
+			//hit_map.clear(); 
 			for(uint x = 0; x < event->count(); x++){
 
 				// Get the samples
@@ -241,29 +289,65 @@ int main(int argc, char **argv)
 
 				// Get the physical channel number
 				channel = Apv25Utils::getPhysicalChannel(samples->apv(), samples->channel());
-
-				// Get the channel baseline
-				baseline = SvtCalibrations::getInstance()->getChannelBaseline(fpga, hybrid, channel);
-				noise = SvtCalibrations::getInstance()->getChannelNoise(fpga, hybrid, channel);
-
-				// Fill the sample array and subtract
-				pass_threshold = 0;
+				
+				// Fill the sample array and subtract the pedestal.
+				pedestal = sig_samples[0]; 
+				threshold_sum = 0;
 				for(int sample_n = 0; sample_n < 6; ++sample_n){
+					pass_threshold[sample_n] = 0; 
 					sig_samples[sample_n] = samples->value(sample_n);
-					sig_samples[sample_n] -= baseline;
-					if(sig_samples[sample_n] >= threshold*noise) pass_threshold++;
+						sig_samples[sample_n] -= SvtCalibrations::getInstance()->getChannelBaseline(fpga, hybrid, channel);
+						noise = SvtCalibrations::getInstance()->getChannelNoise(fpga, hybrid, channel);
 
+					if(sig_samples[sample_n] >= threshold*noise) pass_threshold[sample_n]++;
+				}
+		
+				// Check if any sample is above threshold
+				for(int sample_n = 0; sample_n < 6; ++sample_n){
+					if(pass_threshold[sample_n] > 0) threshold_sum = 1;
+					break; 
 				}
 
-				// Only look for signals with three samples above threshold*noise
-				if(pass_threshold >= 3 && channel > 1 && channel < 637){
+				// Check if two consecutive samples are above threshold
+				for(int sample_n = 0; sample_n < 5; ++sample_n){
+					if(pass_threshold[sample_n] > 0 && pass_threshold[sample_n + 1] > 0) threshold_sum = 2; 
+				}
+
+				// Check if three consecutive samples are above threshold
+				for(int sample_n = 0; sample_n < 4; ++sample_n){ 
+					if(pass_threshold[sample_n] > 0 && pass_threshold[sample_n + 1] > 0 && pass_threshold[sample_n + 2] > 0) threshold_sum = 3; 
+				}
+
+				for(int sample_n = 0; sample_n < 4; ++sample_n){ 
+					if(pass_threshold[sample_n] > 0 && pass_threshold[sample_n + 1] > 0 && pass_threshold[sample_n + 2] > 0 && pass_threshold[sample_n + 3] > 0) threshold_sum = 4; 
+				}
+
+
+				if(threshold_sum >= 1 && channel > 1 && channel < 637){
+					for(int sample_n =0; sample_n <6;sample_n++){
+					h_one_above_thresh->Fill(sig_samples[sample_n]);
+					}
+
+				} 
+			   
+				if(threshold_sum >= 2 && channel > 1 && channel < 637){
+					for(int sample_n =0; sample_n <6;sample_n++){
+					h_two_above_thresh->Fill(sig_samples[sample_n]);
+					}
+				} 
+				
+				if(threshold_sum >= 3 && channel > 1 && channel < 637){
+					
+					for(int sample_n =0; sample_n <6;sample_n++){
+					h_three_above_thresh->Fill(sig_samples[sample_n]);
+					}
 
 					Apv25Utils::fitShaperSignal("Root", sig_samples, fit_params);
 					h_sig_amp->Fill(fit_params[0]);				
 					sig_amp_vs_channel->Fill(channel, fit_params[0]);
 					sig_amps[channel]->Fill(fit_params[0]);
 
-					hit_map.insert(pair<int, double>(channel, fit_params[0]));
+					//hit_map.insert(pair<int, double>(channel, fit_params[0]));
 
 					max_sample = 0;
 					for(int sample_n = 0; sample_n < 6; ++sample_n){
@@ -271,9 +355,15 @@ int main(int argc, char **argv)
 						if(sig_samples[sample_n] > max_sample) max_sample = sig_samples[sample_n];
 					}
 				}
+				
+				if(threshold_sum >= 4 && channel > 1 && channel < 637){
+					for(int sample_n =0; sample_n <6;sample_n++){
+					h_four_above_thresh->Fill(sig_samples[sample_n]);
+					}
+				} 
 			}
 
-			if(hit_map.size() > 0){ 
+			/*if(hit_map.size() > 0){ 
 				h_n_hits->Fill(hit_map.size()); 
 				list<Cluster> clusters = clusterer->findClusters(hit_map);
 				h_n_clusters->Fill(clusters.size());
@@ -289,16 +379,17 @@ int main(int argc, char **argv)
 				
 				}
 
-			}
+			}*/
 		}
 
 		dataRead->close(); 
 	}
 
 	// Setup the file names to output the plots to
-	string output_file_ext = output_file_name.substr(output_file_name.find("."));
-	output_file_name.erase(output_file_name.find("."));
-	output_file_name += "_fpga"  + PlotUtils::convertToString(fpga) + "_hybrid" + PlotUtils::convertToString(hybrid);
+	string output_file_ext = ".pdf"; 
+	output_file_name = "source_run" + PlotUtils::convertToString(run); 
+
+	cout << output_file_name << endl;
 
 	// If a "plots" folder does not exist, create it. This folder will be used
 	// to store any ROOT files generated along with all plots.
@@ -326,9 +417,9 @@ int main(int argc, char **argv)
 		if(entries == 0) continue;
 		sig_amps[channel]->Fit("gaus", "q");
 		g_n_events->SetPoint(channel, channel, entries); 
-		g_gain_stability->SetPoint(channel, channel, sig_amps[channel]->GetFunction("gaus")->GetParameter(1));
+		g_mean_sig_amp->SetPoint(channel, channel, sig_amps[channel]->GetFunction("gaus")->GetParameter(1));
 		error = sig_amps[channel]->GetFunction("gaus")->GetParameter(2)/sqrt(entries); 
-		g_gain_stability->SetPointError(channel, 0, error); 	
+		g_mean_sig_amp->SetPointError(channel, 0, error); 	
 		sig_amps[channel]->Draw();
 		//		PlotUtils::set2DPlotStyle(signal_samples[channel], "Sample Number", "Amplitude [ADC Counts]");
 		canvas->Print((file_path + "/" + output_file_name + "_ch_signal_amplitude" + output_file_ext + "(").c_str());
@@ -340,7 +431,7 @@ int main(int argc, char **argv)
 
 	//--- Fit the signal peak ---//
 	//---------------------------//
-
+/*
 	// Observable
 	RooRealVar amplitude("amplitude", "Fitted Amplitude [ADC Counts]", 0., 1190., "");
 	RooRealVar c_amplitude("c_amplitude", "Single Hit Cluster Fitted Amplitude [ADC Counts]", 0., 1190., "");
@@ -409,7 +500,7 @@ int main(int argc, char **argv)
 	canvas->SetLogy(1);
 	c_frame->Draw(); 
 	canvas->Print((file_path + "/" + output_file_name + output_file_ext + "(").c_str());
-
+*/
 	canvas->SetLogy(0);
 	
 	h_sig_amp->Draw(); 
@@ -422,10 +513,10 @@ int main(int argc, char **argv)
 	canvas->Print((file_path + "/" + output_file_name + output_file_ext + "(").c_str());
 
 
-	g_gain_stability->Draw("A*");
-	PlotUtils::set2DPlotStyle(g_gain_stability, "Physical Channel #", "Fitted Amplitude [ADC Counts]");
-	g_gain_stability->GetYaxis()->SetRangeUser(160, 240); 	
-	g_gain_stability->GetXaxis()->SetRangeUser(0, 640); 	
+	g_mean_sig_amp->Draw("A*");
+	PlotUtils::set2DPlotStyle(g_mean_sig_amp, "Physical Channel #", "Mean Fitted Amplitude [ADC Counts]");
+	g_mean_sig_amp->GetYaxis()->SetRangeUser(160, 400); 	
+	g_mean_sig_amp->GetXaxis()->SetRangeUser(0, 640); 	
 	canvas->Print((file_path + "/" + output_file_name + output_file_ext + "(").c_str());
 
 	g_n_events->Draw("A*"); 	
@@ -442,7 +533,22 @@ int main(int argc, char **argv)
 	canvas->Print((file_path + "/" + output_file_name + output_file_ext + "(").c_str());
 
 	h_n_hits_clusters->Draw();
-	PlotUtils::set1DPlotStyle(h_n_hits_clusters, "Number of Clusters"); 
+	PlotUtils::set1DPlotStyle(h_n_hits_clusters, "Number of Clusters");	
+	canvas->Print((file_path + "/" + output_file_name + output_file_ext + "(").c_str());
+	
+	canvas->SetLogy(1);
+
+	h_one_above_thresh->Draw();
+	h_one_above_thresh->SetLineColor(kRed); 
+
+	h_two_above_thresh->Draw("same");
+	h_two_above_thresh->SetLineColor(kBlue); 
+	
+	h_three_above_thresh->Draw("same");
+	h_three_above_thresh->SetLineColor(kGreen); 
+
+	h_four_above_thresh->Draw("same"); 
+
 	canvas->Print((file_path + "/" + output_file_name + output_file_ext + ")").c_str());
 
 	return EXIT_SUCCESS;
@@ -451,7 +557,7 @@ int main(int argc, char **argv)
 
 void displayUsage()
 {
-	cout << "Usage: apv25_qa [OPTIONS] ..."                 << endl;
+	cout << "Usage: source_analysis [OPTIONS] ..."                 << endl;
 	cout << "Example: apv25_qa -i input_file.bin"           << endl;
 	cout << "\n\t -i    Input file to be processed"         << endl;
 	cout << "\t -o    Optional output file name. By default,"
