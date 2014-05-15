8121269b3ad39513f2a3aaa9bc55272f4841985d
Omar Moreno <email@omarmoreno.net>
2014-05-14 22:59:43 -0700

    Add an interface defining a quality assurance (QA) analysis

---
+++ include/qa/QAAnalysis.h
@@ -0,0 +1,30 @@
+/**
+ *  @author: Omar Moreno <omoreno1@ucsc.edu>
+ *  @section institution
+ *           Santa Cruz Institute for Particle Physics
+ *           University of California, Santa Cruz
+ *  @date:   May 14, 2014
+ *
+ */
+
+#ifndef __QA_ANALYSIS_H__
+#define __QA_ANALYSIS_H__
+
+//--- SVT DAQ ---//
+//---------------//
+#include <TrackerSample.h>
+
+class QAAnalysis { 
+
+    public: 
+
+        virtual ~QAAnalysis(){}; 
+
+        virtual void initialize() = 0; 
+        virtual void processEvent(TrackerSample* samples) = 0;
+        virtual void finalize() = 0;  
+
+}; // QAAanalysis
+
+#endif // __QA_ANALYSIS_H__
+
