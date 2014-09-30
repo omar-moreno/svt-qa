b4ea864a13da915905fc94e2cc729b8218fd5bbf
Omar Moreno <omoreno1@ucsc.edu>
2014-09-30 14:29:11 -0700

    Add method to BaselineAnalysis that fits the baseline distribution and extracts the mean baseline and noise.  Write the results out to an XML file.

--- include/qa/BaselineAnalysis.h
+++ include/qa/BaselineAnalysis.h
@@ -18,11 +18,14 @@
 //--------------//
 #include <QAAnalysis.h>
 #include <QAUtils.h>
+#include <CalibrationWriter.h>
 
 //--- ROOT ---//
 //------------//
 #include <TCanvas.h>
 #include <TH2F.h>
+#include <TF1.h>
+#include <TH1D.h>
 
 class BaselineAnalysis : public QAAnalysis { 
 
@@ -37,16 +40,21 @@ class BaselineAnalysis : public QAAnalysis {
         void finalize();
 		std::string toString(); 
 
+        void findCalibrations(TH1*, double &, double &);
+
     private: 
 
         // TODO: Use a histogram factory to handle booking of histograms directly
         TCanvas* canvas;
         TH2F* baseline_h; 
+        TF1* gaussian;         
+
+        CalibrationWriter* writer; 
 
 		std::string class_name;
 
 		int feb_id; 
-		int hybrid_id;	
+		int hybrid_id;
 };
 
 #endif // __BASELINE_ANALYIS_H__
--- src/qa/BaselineAnalysis.cxx
+++ src/qa/BaselineAnalysis.cxx
@@ -10,14 +10,19 @@
 #include <BaselineAnalysis.h>
 
 BaselineAnalysis::BaselineAnalysis()
-	: canvas(NULL), baseline_h(NULL),
+	: canvas(NULL), baseline_h(NULL), gaussian(NULL),
+      writer(new CalibrationWriter()),
 	  class_name("BaselineAnalysis"), 
 	  feb_id(0), hybrid_id(0)
 {}
 
 BaselineAnalysis::BaselineAnalysis(int feb_id, int hybrid_id)
+	: canvas(NULL), baseline_h(NULL), gaussian(NULL),
+      writer(new CalibrationWriter()),
+	  class_name("BaselineAnalysis"), 
+	  feb_id(0), hybrid_id(0)
 {
-	this->feb_id = feb_id; 
+    this->feb_id = feb_id; 
 	this->hybrid_id = hybrid_id; 
 }
 
@@ -25,6 +30,9 @@ BaselineAnalysis::~BaselineAnalysis()
 {
     delete canvas; 
     delete baseline_h;
+    delete gaussian;
+
+    delete writer;  
 }
 
 void BaselineAnalysis::initialize()
@@ -32,6 +40,13 @@ void BaselineAnalysis::initialize()
     // TODO: Use a histogram factory instead of booking histograms directly
 	canvas = new TCanvas("canvas", "canvas", 300, 300);
 	baseline_h = new TH2F("baseline", "Baseline", 640, 0, 640, 16384, 0, 16384);
+    gaussian = new TF1("gaussian", "gaus");
+
+    writer->openDocument("test.xml");
+    // TODO: Add method to writer that checks whether a specific FEB and Hybrid 
+    //       node have been created.
+    writer->addFeb(feb_id);
+    writer->addHybrid(feb_id, hybrid_id); 
     
 }
 
@@ -45,17 +60,34 @@ void BaselineAnalysis::processEvent(TriggerSample* samples)
 	int physical_channel = QAUtils::getPhysicalChannel(samples->apv(), samples->channel()); 
 	
 	for(int sample_n = 0; sample_n < 6; ++sample_n){
-		baseline_h->Fill(physical_channel, double(samples->value(sample_n)));
+            baseline_h->Fill(physical_channel, double(samples->value(sample_n)));
 	}
 }
 
 void BaselineAnalysis::finalize()
 {
 
+    TH1D* projection = NULL;
+    double baseline, noise; 
+    for(int channel = 0; channel < 640; ++channel){
+        projection = baseline_h->ProjectionY("projection", channel+1, channel+1, "e");
+        if(projection->GetEntries() == 0){
+            std::cout << "[ BaselineAnalysis ]: Histogram has not entries." << std::endl;
+            continue; 
+        }
+        findCalibrations(projection, baseline, noise);
+        writer->writeBaseline(feb_id, hybrid_id, 0, channel, 0, baseline);  
+        writer->writeNoise(feb_id, hybrid_id, 0, channel, 0, noise);
+        //cout << "[ BaselineAnalysis ]: Baseline: " << baseline << " Noise: " << noise << endl; 
+    
+    } 
+
     canvas->Print("baseline_run_summary.pdf[");
     baseline_h->Draw("colz");
 	canvas->Print("baseline_run_summary.pdf(");
     canvas->Print("baseline_run_summary.pdf]");
+
+    writer->closeDocument();
 }
 
 std::string BaselineAnalysis::toString()
@@ -64,6 +96,14 @@ std::string BaselineAnalysis::toString()
 	return string_buffer; 	
 }
 
+void BaselineAnalysis::findCalibrations(TH1* pedestal_plot, double &baseline, double &noise)
+{
+   gaussian->SetRange(pedestal_plot->GetMean() - 3*pedestal_plot->GetRMS(), 
+                      pedestal_plot->GetMean() + 3*pedestal_plot->GetRMS());
+    pedestal_plot->Fit("gaussian", "RQ"); 
+    baseline = gaussian->GetParameter(1); 
+    noise = gaussian->GetParameter(2); 
+}
 
 
 
