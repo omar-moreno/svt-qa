69b73ef0e921f1d7b77a88eda85fc62470192fe3
Omar Moreno <email@omarmoreno.net>
2014-11-12 08:05:40 -0800

    Loop through the samples and get sample values.

--- test/DataReadEvioTest.cxx
+++ test/DataReadEvioTest.cxx
@@ -1,6 +1,7 @@
 
 #include <DataReadEvio.h>
 #include <TestRunSvtEvent.h>
+#include <TestRunSvtTrackerSample.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include <Data.h>
@@ -10,7 +11,7 @@ using namespace std;
 int main(int argc, char **argv)	
 {
 	string evio_file_name;
-	
+
 	// Parse all the command line arguments. If there are no valid command line
 	// arguments passed, print the usage and exit.
 	static struct option long_options[] = 
@@ -37,14 +38,34 @@ int main(int argc, char **argv)
 		cerr << "[ SVT QA ]: Use --help for usage.\n" << endl;
 		return EXIT_FAILURE; 
 	}
-	
+
 	DataReadEvio* data_reader = new DataReadEvio();
 	data_reader->open(evio_file_name, false);
-	TestRunSvtEvent* data = new TestRunSvtEvent(); 
+	TestRunSvtTrackerSample* data_sample = NULL;
+	TestRunSvtEvent* data = new TestRunSvtEvent();
+
+
+//	TestRunSvtTrackerSample* data_sample = new TestRunSvtTrackerSample(); 
 
 	while(data_reader->next(data)){
-	  cout << "Fpga: " << data->fpgaAddress() << endl;	
+		cout << "[ DataReadEvioTest ]:\t\tFpga: " << data->fpgaAddress() << endl;
 		
+		for(int temp_index = 0; temp_index < 12; ++temp_index){
+			cout << "[ DataReadEvioTest ]:\t\tTemperature " << temp_index << ": " 
+				 << data->temperature(temp_index) << endl; 
+		}
+		cout << "[ DataReadEvioTest ]:\t\tCount: " << data->count() << endl;
+		for(uint sample_set_n = 0; sample_set_n < data->count(); ++ sample_set_n){ 
+			data->sample(sample_set_n, data_sample);
+			cout << "[ DataReadEvioTest ]:\t\tFPGA:    " << data_sample->fpgaAddress() << endl;
+			cout << "[ DataReadEvioTest ]:\t\tHybrid:  " << data_sample->hybrid() << endl;
+			cout << "[ DataReadEvioTest ]:\t\tapv:	   " << data_sample->apv() << endl;
+			cout << "[ DataReadEvioTest ]:\t\tchannel: " << data_sample->channel() << endl;
+			for(int sample_n = 0; sample_n < 6; ++sample_n){
+				cout << "[ DataReadEvioTest ]:\t\t\tsample " << sample_n 
+					 << ": " << data_sample->value(sample_n) << endl;
+			}
+		}
 	}
 	data_reader->close();
 }
