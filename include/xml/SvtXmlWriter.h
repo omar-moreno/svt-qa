153756f92e3ad652fd0a428db0a754f893660115
Omar Moreno <omoreno1@ucsc.edu>
2014-12-01 13:46:33 -0800

    Update to use FebNode objects.  Add class documentation.

--- include/xml/SvtXmlWriter.h
+++ include/xml/SvtXmlWriter.h
@@ -1,47 +1,72 @@
+/**
+ * 
+ *  @file   SvtXmlWriter.h
+ *  @brief  XML writer used to write SVT conditions.
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          University of California, Santa Cruz
+ *          Santa Cruz Institute for Particle Physics
+ *  @date   September 15, 2014  
+ */
 
 #ifndef __SVT_XML_WRITER_H__
 #define __SVT_XML_WRITER_H__
 
-//--------------//
-//--- StdLib ---//
-//--------------//
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <unordered_map>
+#include <stdexcept>
 
 //--------------//
 //--- LibXML ---//
 //--------------//
 #include <libxml/tree.h>
 
+//--------------//
+//--- SVT QA ---//
+//--------------//
+#include <FebNode.h>
+
 class SvtXmlWriter { 
 
 	public: 
 
+        /**
+         *  Default Constructor
+         */
 		SvtXmlWriter(); 
+
+        /**
+         *  Destructor
+         */
 		virtual ~SvtXmlWriter(); 
 
-		void openDocument(std::string);
-		void closeDocument();
+        /**
+         *  Open the SVT XML document
+         *
+         *  @param file_name - The name of the XML document
+         */
+		void open(std::string file_name);
+		
+        /**
+         *  Close the SVT XML document
+         */
+        void close();
 
-		void addFeb(int);
-		void addHybrid(int, int); 
-
-		void addChannelData(std::string, int, int, int, int, int, double);	
+        /**
+         *  Get the FebNode corresponding to the given FEB ID
+         *
+         *  @param feb_id - The FEB ID of the FebNode of interest
+         *  @return The FebNode corresponding to the given ID 
+         */
+        FebNode* getFebNode(int feb_id);
 
 	private:
-
-		// Method to convert any value to a string 
-		template<typename T>
-			std::string numberToString(const T value)
-			{
-				std::ostringstream ss;
-				ss << value;
-				return ss.str();  
-			}
 
 		// Pointer to the XML document
 		xmlDocPtr document; 
@@ -52,8 +77,8 @@ class SvtXmlWriter {
 		// Pointer to the config element
 		xmlNodePtr config_node; 
 
-		// A map between a FEB ID and the corresponding xmlNodePtr
-		std::map<int, xmlNodePtr> feb_nodes; 
+        // A mapping between a FEB ID and the corresponding FEB node
+        std::unordered_map <int, FebNode*> feb_nodes;
 
 		// Name of the file the document will be saved to
 		std::string file_name;
--- src/xml/SvtXmlWriter.cxx
+++ src/xml/SvtXmlWriter.cxx
@@ -1,3 +1,12 @@
+/**
+ * 
+ *  @file   SvtXmlWriter.cxx
+ *  @brief  XML writer used to write SVT conditions.
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          University of California, Santa Cruz
+ *          Santa Cruz Institute for Particle Physics
+ *  @date   September 15, 2014  
+ */
 
 #include <SvtXmlWriter.h>
 
@@ -7,11 +16,11 @@ SvtXmlWriter::SvtXmlWriter()
 	  bytes_written(-1)	  
 {}
 
-SvtXmlWriter::~SvtXmlWriter()
-{}
+SvtXmlWriter::~SvtXmlWriter() {
+    feb_nodes.clear();
+}
 
-void SvtXmlWriter::openDocument(std::string file_name)
-{
+void SvtXmlWriter::open(std::string file_name) {
 
 	LIBXML_TEST_VERSION;
 
@@ -21,10 +30,12 @@ void SvtXmlWriter::openDocument(std::string file_name)
 	// Set the file name that the document will be saved to
 	this->file_name = file_name; 
 
-	//--- Build and SVT document ---//
+	//--- Build the SVT document ---//
 	//------------------------------//
 	
-	// Create and set the root node "system" 
+	// Create and set the root node "system"
+    // TODO: Check whether this will still be the name assigned to the root
+    //       node 
 	system_node = xmlNewNode(NULL, BAD_CAST "system");
 	xmlDocSetRootElement(document, system_node);
 
@@ -34,155 +45,39 @@ void SvtXmlWriter::openDocument(std::string file_name)
 }
 
 
-void SvtXmlWriter::closeDocument()
-{
-	// If a document hasn't been created, notify the user and exit
+void SvtXmlWriter::close() {
+
+    // If a document hasn't been created, throw a runtime exception
 	if(document == NULL){
-		std::cerr << "[ERROR]: A document hasn't been created." << std::endl;
-		return;
+		throw std::runtime_error("[ ERROR ]: A document hasn't been created.");
 	}
 
 	// Save the document to the file name specified by the user.  If the file
 	// can't be saved, notify the user and exit.
 	bytes_written = xmlSaveFormatFile(file_name.c_str(), document, 1); 
 	if(bytes_written < 0){
-		std::cerr << "[ERROR]: Could not write file to " << file_name << "." << std::endl; 
-		return;
+	    throw std::runtime_error("[ ERROR ]: Could not write file to "
+                                    + file_name + "."); 
 	}
 
 	// Free the document
 	xmlFreeDoc(document); 
 }
 
+FebNode* SvtXmlWriter::getFebNode(int feb_id) { 
+  
+    // If a document hasn't been created, throw a runtime exception
+    if (document == NULL) { 
+        throw std::runtime_error("[ ERROR ]: Cannot created a FEB node. The document has not been opened."); 
+    }
 
-void SvtXmlWriter::addFeb(int feb_id)
-{	
-	if(document == NULL || system_node == NULL || config_node == NULL){
-		std::cerr << "[ERROR]: Cannot create FEB node. Either the system/configuration nodes "
-			      << "have not been created or the document has not been opened." << std::endl;
-		return;
-	}
-	
-	// Create a FEB node as a child of the config node
-	xmlNodePtr feb_node = xmlNewChild(config_node, NULL, BAD_CAST "FrontEndTestFpga", NULL);
-	feb_nodes.insert(std::pair<int,xmlNodePtr>(feb_id, feb_node)); 
+    // Check if the FebNode already exists.  If it doesn't, create it.
+    if (feb_nodes.find(feb_id) == feb_nodes.end()) { 
+        feb_nodes[feb_id] = new FebNode(config_node, feb_id);
+        std::cout << "[ SvtXmlWriter ]: FebNode with ID " << feb_id 
+                  << " was created." << std::endl;
+   
+    }
 
-	// Add the FEB ID to FEB node as an attribute
-	xmlNewProp(feb_nodes[feb_id], BAD_CAST "index", 
-				BAD_CAST numberToString<int>(feb_id).c_str()); 
+    return feb_nodes[feb_id];
 }
-
-void SvtXmlWriter::addHybrid(int feb_id, int feb_hybrid_id)
-{
-	if(document == NULL || system_node == NULL || config_node == NULL){
-		std::cerr << "[ERROR]: Cannot create FEB node. Either the system/configuration nodes "
-			      << "have not been created or the document has not been opened." << std::endl;
-		return;
-	}
-
-	// Get the FEB node associated with the given hybrid.  If the FEB node doesn't exist, 
-	// create it
-	std::map<int, xmlNodePtr>::iterator node_iterator = feb_nodes.find(feb_id); 
-	if(node_iterator == feb_nodes.end()){
-		// Create a FEB node with the appropriate identifier
-		addFeb(feb_id);  
-	}
-	xmlNodePtr feb_node = feb_nodes[feb_id]; 
-
-	// Create a hybrid node as a child of the FEB node
-	xmlNodePtr hybrid_node = xmlNewChild(feb_node, NULL, BAD_CAST "Hybrid", NULL);
-	
-	// Add the hybrid ID to the hybrid node as an attribute
-	xmlNewProp(hybrid_node, BAD_CAST "index", 
-				BAD_CAST numberToString<int>(feb_hybrid_id).c_str());
-
-	// Create 5 APV25 nodes as children of the hybrid node
-	xmlNodePtr apv_node = NULL;
-	for(int apv_n = 0; apv_n < 5; ++apv_n){
-		apv_node = 	xmlNewChild(hybrid_node, NULL, BAD_CAST "apv25", NULL);
-
-		// Add the APV25 index to the APV25 node as an attribute
-		xmlNewProp(apv_node, BAD_CAST "index", BAD_CAST numberToString<int>(apv_n).c_str());
-	
-		// Create 128 channel nodes as children of the apv25 node
-		xmlNodePtr channel_node = NULL;
-		for(int channel_n = 0; channel_n < 128; ++channel_n){
-			channel_node = 	xmlNewChild(apv_node, NULL, BAD_CAST "channel", NULL);
-
-			// Add the channel index to the channel node as an attribute
-			xmlNewProp(channel_node, BAD_CAST "index", 
-						BAD_CAST numberToString<int>(channel_n).c_str());
-		}	
-	}
-}
-
-void SvtXmlWriter::addChannelData(std::string type, 
-		int feb_id, int feb_hybrid_id, int apv, int channel, int sample, double value)
-{
-	if(document == NULL || system_node == NULL || config_node == NULL){
-		std::cerr << "[ERROR]: Cannot create FEB node. Either the system/configuration nodes "
-			      << "have not been created or the document has not been opened." << std::endl;
-	}
-
-	// Get the FEB node associated with the channel data.  If the FEB node doesn't exist, 
-	// notify the user and exit
-	std::map<int, xmlNodePtr>::iterator node_iterator = feb_nodes.find(feb_id); 
-	if(node_iterator == feb_nodes.end()){
-		std::cerr << "[ERROR]: Cannot add channel data. " 
-				  << "The FEB node has not been created." << std::endl;
-	
-		return;
-	}
-	xmlNodePtr feb_node = feb_nodes[feb_id]; 
-	
-	// Loop through all of the childen (Hybrid nodes) of the feb_node and get
-	// the hybrid node whose id matches the given id.
-	xmlChar* index = NULL;
-	xmlNodePtr hybrid_node = feb_node->children;
-	index = xmlGetProp(feb_node, BAD_CAST "index");
-	while(hybrid_node != NULL){
-		index = xmlGetProp(hybrid_node, BAD_CAST "index");
-		if(xmlStrEqual(index, BAD_CAST numberToString<int>(feb_hybrid_id).c_str())){
-			xmlFree(index);
-			break;
-		}
-		hybrid_node = hybrid_node->next;
-	}	
-	
-	// Loop through all of the children (apv25 nodes) of the hybrid_node and get
-	// the apv25 node whose id matches the apv25 number
-	xmlNodePtr apv25_node = hybrid_node->children;
-	while(apv25_node != NULL){
-		index = xmlGetProp(apv25_node, BAD_CAST "index");
-		if(xmlStrEqual(index, BAD_CAST numberToString<int>(apv).c_str())){
-			xmlFree(index);
-			break;
-		}
-		apv25_node = apv25_node->next;
-	}
-
-	// Loop through all of the children (channel nodes) of the apv25_node and
-	// get the channel node whose id matches the channel number.
-	xmlNodePtr channel_node = apv25_node->children;
-	while(channel_node != NULL){
-		index = xmlGetProp(channel_node, BAD_CAST "index");
-		if(xmlStrEqual(index, BAD_CAST numberToString<int>(channel).c_str())){
-			xmlFree(index);
-			break;
-		}
-		channel_node = channel_node->next;
-	}
-
-	// Add a node of the given type to the channel along with the specified
-	// value
-	xmlNodePtr data_node = xmlNewChild(channel_node, NULL, 
-			BAD_CAST type.c_str(), BAD_CAST numberToString<double>(value).c_str());
-	// If a sample number is specified, add the sample number as an attribute
-	if(sample != -1){
-		// Add the channel index to the channel node as an attribute
-		xmlNewProp(data_node, BAD_CAST "index", 
-					BAD_CAST numberToString<int>(sample).c_str());
-	}
-}
-
-
