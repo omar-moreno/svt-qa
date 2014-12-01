e0e39b625d17c7772386405a9426ee3cffa864ff
Omar Moreno <omoreno1@ucsc.edu>
2014-12-01 15:24:34 -0800

    Update to reflect the changes made to SvtXmlWriter.  Added some documentation.

--- include/xml/CalibrationWriter.h
+++ include/xml/CalibrationWriter.h
@@ -1,26 +1,65 @@
 /**
  *
+ *  @file   CalibrationWriter.cxx 
+ *  @brief  XML writer used to write SVT baseline and noise conditions.
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          University of California, Santa Cruz
+ *          Santa Cruz Institute for Particle Physics
+ *  @date   September 15, 2014 
  */
+
 
 #ifndef __CALIBRATION_WRITER__
 #define __CALIBRATION_WRITER__
 
-//--------------//
-//--- StdLib ---//
-//--------------//
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <string>
 
+//--------------//
+//--- SVT QA ---//
+//--------------//
 #include <SvtXmlWriter.h>
 
 class CalibrationWriter : public SvtXmlWriter { 
 
 	public:
 
+        /**
+         *  Default Constructor
+         */
 		CalibrationWriter();
+
+        /**
+         *  Destructor
+         */
 		~CalibrationWriter();
 
-		void writeBaseline(int, int, int, int, int, double);
-		void writeNoise(int, int, int, int, int, double);
-};
+        /**
+         *  Write a baseline value to the XML document
+         *
+         *  @param feb_id - The FEB ID associated with the baseline value
+         *  @param hybrid_id - The hybrid ID associated with the baseline value
+         *  @param channel - The channel associated with the baseline value
+         *  @param sample - The sample number associated with the baseline value
+         *  @param baseline - The baseline value
+         */
+		void writeBaseline(int feb_id, int hybrid_id, int channel,
+                           int sample, double baseline);
+		
+        /**
+         *  Write a noise value to the XML document
+         *
+         *  @param feb_id - The FEB ID associated with the baseline value
+         *  @param hybrid_id - The hybrid ID associated with the baseline value
+         *  @param channel - The channel associated with the baseline value
+         *  @param sample - The sample number associated with the baseline value
+         *  @param noise - The noise value
+         */
+        void writeNoise(int feb_id, int hybrid_id, int channel, 
+                        int sample, double noise);
+
+}; // CalibrationWriter
 
 #endif // __CALIBRATION_WRITER__
--- src/xml/CalibrationWriter.cxx
+++ src/xml/CalibrationWriter.cxx
@@ -1,21 +1,28 @@
+/**
+ *
+ *  @file   CalibrationWriter.cxx 
+ *  @brief  XML writer used to write SVT baseline and noise conditions.
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          University of California, Santa Cruz
+ *          Santa Cruz Institute for Particle Physics
+ *  @date   September 15, 2014 
+ */
 
 #include <CalibrationWriter.h>
 
-CalibrationWriter::CalibrationWriter() 
-{}
+CalibrationWriter::CalibrationWriter() {}
 
-CalibrationWriter::~CalibrationWriter()
-{}
+CalibrationWriter::~CalibrationWriter() {}
 
-void CalibrationWriter::writeBaseline(int feb_id, int feb_hybrid_id, 
-		int apv, int channel, int sample, double value)
-{
-	addChannelData("baseline", feb_id, feb_hybrid_id, apv, channel, sample, value);
+void CalibrationWriter::writeBaseline(int feb_id, int hybrid_id, int channel,
+                                      int sample, double baseline) {
+    this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
+            "baseline", channel, sample, baseline);
 }
 
-void CalibrationWriter::writeNoise(int feb_id, int feb_hybrid_id, 
-		int apv, int channel, int sample, double value)
-{
-	addChannelData("noise", feb_id, feb_hybrid_id, apv, channel, sample, value);
+void CalibrationWriter::writeNoise(int feb_id, int hybrid_id, int channel,
+                                   int sample, double noise) {
+    this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
+            "noise", channel, sample, noise);
 }
 
