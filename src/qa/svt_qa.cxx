3803ff17d1368a4527796d1359418287bae6275f
Omar Moreno <omoreno1@ucsc.edu>
2014-12-02 15:36:35 -0800

    Add support to run over multiple files.  The files are passed to svt_qa as a list in a text file.

--- src/qa/svt_qa.cxx
+++ src/qa/svt_qa.cxx
@@ -6,14 +6,17 @@
  *  @date   May 5, 2014
  */
 
-//--- C++ ---//
-//-----------//
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <iostream>
-#include <stdlib.h>
+#include <fstream>
+#include <cstdlib>
 #include <list>
 #include <getopt.h>
-#include <time.h>
+#include <ctime>
 
+//---------------//
 //--- SVT DAQ ---//
 //---------------//
 #include <TriggerEvent.h>
@@ -21,6 +24,7 @@
 #include <DataRead.h>
 #include <Data.h>
 
+//--------------//
 //--- SVT QA ---//
 //--------------//
 #include <DataReadEvio.h>
@@ -35,6 +39,7 @@ int main(int argc, char **argv) {
     clock_t initial_time = clock(); 
     
     string input_file_name;
+    string input_file_list_name;
     bool run_baseline = false;
     bool evio = false; 
     int feb_id = -1; 
@@ -45,6 +50,7 @@ int main(int argc, char **argv) {
     // arguments passed, print the usage and exit.
     static struct option long_options[] = {
         { "input",          required_argument, 0, 'i' },
+        { "input_list",     required_argument, 0, 'l' },
         { "feb",            required_argument, 0, 'f' },
         { "hybrid",         required_argument, 0, 'h' },
         { "total_events",   required_argument, 0, 'n' },
@@ -55,10 +61,13 @@ int main(int argc, char **argv) {
     };
     int option_index = 0;
     int option_char; 
-    while ((option_char = getopt_long(argc, argv, "i:f:h:n:beu", long_options, &option_index)) != -1) {
+    while ((option_char = getopt_long(argc, argv, "i:l:f:h:n:beu", long_options, &option_index)) != -1) {
         switch (option_char) {
             case 'i': 
                 input_file_name = optarg; 
+                break;
+            case 'l': 
+                input_file_list_name = optarg;
                 break;
             case 'f': 
                 feb_id = atoi(optarg);
@@ -86,10 +95,31 @@ int main(int argc, char **argv) {
 
     // If an input file (binary or EVIO) was not specified, warn the user and 
     // exit the application
-    if (input_file_name.empty()) {
-        cerr << "\n[ SVT QA ]: Please specify a file to process." << endl;
+    if (input_file_name.empty() && input_file_list_name.empty()) {
+        cerr << "\n[ SVT QA ]: Please specify a file or a list of files to process." << endl;
         cerr << "[ SVT QA ]: Use --help for usage.\n" << endl;
         return EXIT_FAILURE; 
+    } else if (!input_file_name.empty() && !input_file_list_name.empty()) { 
+        cerr << "\n[ SVT QA ]: Cannot specify both a input file name and a list of files." << endl;
+        cerr << "[ SVT QA ]: Use --help for usage.\n" << endl;
+        return EXIT_FAILURE; 
+    }
+
+    // Create the list of files to process
+    list<string> files; 
+    string file; 
+    if(!input_file_name.empty()) { 
+        files.push_back(input_file_name); 
+    } else if (!input_file_list_name.empty()) { 
+        ifstream input_file_list(input_file_list_name.c_str(), ifstream::in);
+        if(!input_file_list.is_open()) { 
+            cerr << "[ SVT QA ]: Failed to open file " << input_file_list << endl;
+            return EXIT_FAILURE;
+        }
+        while (input_file_list >> file) { 
+            files.push_back(file); 
+        }
+        input_file_list.close(); 
     }
 
     // 
@@ -102,18 +132,9 @@ int main(int argc, char **argv) {
         data_reader = new DataRead(); 
     }
     
-    // Open the input file.  If the input file can't be opened, exit the 
-    // application
-    if (!data_reader->open(input_file_name)) {
-        cerr << "\n[ SVT QA ]: Error! File " << input_file_name << " cannot be opened." << endl;
-        return EXIT_FAILURE; 
-    }
-
     // Container to hold all analyses 
     list<QAAnalysis*> analyses; 
-
-    cout << "[ SVT QA ]: Processing file: " << input_file_name << endl;
-
+    
     // TODO: All analyses should be loaded dynamically
     if (run_baseline) {
         if (feb_id != -1 && hybrid_id != -1) 
@@ -127,24 +148,36 @@ int main(int argc, char **argv) {
         (*analysis)->initialize(); 
     }   
     
-    int event_number = 0; 
-    int channel;
-    while (data_reader->next(&trigger_event)) {
-        if (event_number == total_events) break; 
-        ++event_number;
+    int event_number = 0;
+    // Loop over all input files and process them
+    for (list<string>::iterator files_it = files.begin(); files_it != files.end(); ++files_it) { 
+   
+        // Open the input file.  If the input file can't be opened, exit the 
+        // application
+        if (!data_reader->open(*files_it)) {
+            cerr << "\n[ SVT QA ]: Error! File " << *files_it << " cannot be opened." << endl;
+            return EXIT_FAILURE; 
+        }
+        cout << "[ SVT QA ]: Processing file: " << *files_it << endl;
+        event_number = 0;  
+        while (data_reader->next(&trigger_event)) {
+            if (event_number == total_events) break; 
+            ++event_number;
 
-        if(event_number%500 == 0) 
-            cout << "[ SVT QA ]: Processing event " << event_number << endl;
+            if(event_number%500 == 0) 
+                cout << "[ SVT QA ]: Processing event " << event_number << endl;
 
-        for (uint sample_set_n = 0; sample_set_n < trigger_event.count(); ++sample_set_n) {
+            for (uint sample_set_n = 0; sample_set_n < trigger_event.count(); ++sample_set_n) {
             
-            trigger_event.sample(sample_set_n, trigger_samples);
+                trigger_event.sample(sample_set_n, trigger_samples);
                         
-            for (list<QAAnalysis*>::iterator analysis = analyses.begin(); analysis != analyses.end(); ++analysis) {
-                (*analysis)->processEvent(trigger_samples); 
-            }   
+                for (list<QAAnalysis*>::iterator analysis = analyses.begin(); analysis != analyses.end(); ++analysis) {
+                    (*analysis)->processEvent(trigger_samples); 
+                }   
+            }
         }
     }
+
   
     for (list<QAAnalysis*>::iterator analysis = analyses.begin(); analysis != analyses.end(); ++analysis) {
         (*analysis)->finalize(); 
