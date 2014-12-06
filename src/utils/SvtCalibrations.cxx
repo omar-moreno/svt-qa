603c7bad4b0844af3c395ddb621c4b0123164f8f
Omar Moreno <omoreno1@ucsc.edu>
2014-12-05 17:12:30 -0800

    Update SvtCalibrations to load SVT conditions (baseline and noise) from an XML file instead of a text file.

--- src/utils/SvtCalibrations.cxx
+++ src/utils/SvtCalibrations.cxx
@@ -1,80 +1,78 @@
 /**
- *	SvtCalibrations.cpp
- *
- *		@author:  Omar Moreno <omoreno1@ucsc.edu>
- *		@version: v 1.0
+ *  @file SvtCalibrations.cxx
+ *  @brief Singleton class used to load and retrieve SVT calibrations
+ *	@author Omar Moreno <omoreno1@ucsc.edu>
+ *	        Santa Cruz Institute for Particle Physics
+ *	        University of California, Santa Cruz
+ *	@date   March 05, 2014
  */
 
-#include "SvtCalibrations.h"
+#include <SvtCalibrations.h>
 
 SvtCalibrations* SvtCalibrations::instance = NULL;
 
-bool SvtCalibrations::calibrations_loaded = false;
+SvtCalibrations::SvtCalibrations()
+    : reader(new SvtXmlReader()),
+      calibrations_loaded(false),
+      xml_file_name("")
+{}
 
-SvtCalibrations::~SvtCalibrations()
-{
+SvtCalibrations::~SvtCalibrations() {
 	delete instance;
+	delete reader;
+	baseline_map.clear();
+	noise_map.clear();
 }
 
-SvtCalibrations* SvtCalibrations::getInstance()
-{
-	if(instance == NULL){
+SvtCalibrations* SvtCalibrations::getInstance() {
+
+    if(instance == NULL){
 		instance = new SvtCalibrations();
-		return instance;
-	} else return instance;
+	}
+	return instance;
 }
 
-bool SvtCalibrations::loadCalibrations(std::string baseline_path)
-{
-	// If the calibrations have already been loaded, don't bother
-	// reloading them
-	if(calibrations_loaded) return true;
+bool SvtCalibrations::loadCalibrations(std::string xml_file_path) {
 
-	// Check if the specified file is valid.  Otherwise continue by
-    // using the first sample as the pedestal
-    std::ifstream baseline_file;
-    baseline_file.open(baseline_path.c_str());
-    if(!baseline_file){
-    	std::cerr << "Unable to open " << baseline_path.c_str() << "!" << std::endl;
-    	std::cerr << "First sample will be used as the pedestal." << std::endl;
-    	return false;
-    }
-    
-	// Loop through each line in the calibration file
-    std::string line;
-    int fpga, hybrid, channel;
-    double baseline, noise;
-	std::map<std::pair<int, int>, std::vector<double> >::iterator baseline_it;
-    while(std::getline(baseline_file,line)){
+    // If the calibrations have already been loaded and the file name hasn't
+    // changed, don't bother reloading them
+	if(calibrations_loaded && this->xml_file_name == xml_file_name) return true;
 
-    	// If an line is commented out, skip it
-    	if(int(line.find("#")) == 0) continue;
+	// Set the file name
+	this->xml_file_name = xml_file_name;
 
-    	// Extract the calibration values from the stream
-    	std::istringstream stream(line);
-    	stream >> fpga >> hybrid >> channel >> baseline >> noise;
+    // Open the file using the SVT XML reader
+    reader->open(xml_file_name);
 
-		// Check if the maps contain the correct fpga/hybrid pair
-    	// Load the baseline and noise maps with the calibration variables
-    	std::pair<int, int> daq_pair = std::make_pair(fpga, hybrid);
-		baseline_it = baseline_map.find(daq_pair); 
-		if(baseline_it == baseline_map.end()){
-    		baseline_map[daq_pair] = std::vector<double>(640);
-    		noise_map[daq_pair] = std::vector<double>(640);
-		}
+    // Get the list of FebNodes from the reader and iterate over them
+    std::vector<FebNode*> feb_nodes = reader->getFebNodes();
+	std::vector<FebNode*>::iterator feb_node = feb_nodes.begin();
+	for (feb_node; feb_node != feb_nodes.end(); ++feb_node) {
+	    for (int hybrid_id = 0; hybrid_id < 4; ++hybrid_id) {
 
-    	baseline_map[daq_pair][channel] = baseline;
-    	noise_map[daq_pair][channel] = noise;
+	        // Check if the baseline map already contains the DAQ pair key.
+	        // If it doesn't, add a container to contain the calibrations for
+	        // that sensor
+	        std::pair<int, int> daq_pair = std::make_pair((*feb_node)->getFebID(), hybrid_id);
+	        if (baseline_map.find(daq_pair) == baseline_map.end()) {
+	            baseline_map[daq_pair] = std::vector<double>(640);
+	            noise_map[daq_pair] = std::vector<double>(640);
+	        }
 
-		/*
-		std::cout << "FPGA: " << fpga << " Hybrid: " << hybrid 
-				  << " Channel: " << channel 
-				  << " Baseline: " << baseline_map[daq_pair][channel] 
-				  << std::endl;
-		*/
-    }
+	        // Loop through all of the channels and samples and retrieve the
+	        // calibrations
+	        for (int channel = 0; channel < 640; ++channel) {
+	            for (int sample = 0; sample < 6; ++sample) {
+	                baseline_map[daq_pair][channel]
+	                            = (*feb_node)->getHybridNode(hybrid_id)->getChannelData("baseline", channel, sample);
+	                noise_map[daq_pair][channel]
+	                            =(*feb_node)->getHybridNode(hybrid_id)->getChannelData("noise", channel, sample);
+	            }
+	        }
+	    }
+	}
 
-    std::cout << "Calibrations have been loaded." << std::endl;
+    std::cout << "[ SvtCalibrations ]: Calibrations have been loaded." << std::endl;
     calibrations_loaded = true;
     return true;
 }
