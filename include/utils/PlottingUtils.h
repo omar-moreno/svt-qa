16eafcc22d9c8009b5dddf48800b921468666fe5
Omar Moreno <omoreno1@ucsc.edu>
2014-12-10 11:34:10 -0800

    Rename the methods used to instantiate multiple 1D and 2D ROOT histograms.  Added documentation.

--- include/utils/PlottingUtils.h
+++ include/utils/PlottingUtils.h
@@ -11,14 +11,17 @@
 #ifndef __PLOTTING_UTILS_H__
 #define __PLOTTING_UTILS_H__
 
-#include <iostream>
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
+#include <string>
 #include <sstream>
-#include <typeinfo>
 
+//------------//
 //--- ROOT ---//
+//------------//
 #include <TColor.h>
 #include <TStyle.h>
-
 
 namespace PlottingUtils { 
 
@@ -29,7 +32,7 @@ namespace PlottingUtils {
     void setPalette(); 
    
 	/**
-	 *
+	 *  Setup the style used by ROOT
 	 */
 	void setStyle(); 
 
@@ -41,60 +44,107 @@ namespace PlottingUtils {
 	 *	@return A string object representation of value
 	 */
     template <typename T>
-        std::string toString(const T value) {
+    std::string toString(const T value) {
             std::stringstream ss; 
             ss << value; 
             return ss.str(); 
-        } 
+    }
 
-	/**
-	 *
-	 */
-	template <typename T>
-		void initMulti1DPlots(int n_plots, std::vector<T*> &plots, std::string hist_name, int bins_x, int x_min, int x_max)
-		{
-            std::string h_name; 
-            for(int plot_n = 0; plot_n < n_plots; ++plot_n){
-                h_name = hist_name + "_" + toString<int>(plot_n); 
-                plots.push_back(new T(h_name.c_str(), h_name.c_str(), bins_x, x_min, x_max));          
-            }
-		
-		}	
-
-	/**
-	 *
-	 */
-    template <typename T> 
-        void initMulti1DPlots(int n_plots, std::vector<T*> &plots, std::string hist_name, int x_min, int x_max)
-        {        
-            int bins_x = x_max - x_min;
-			initMulti1DPlots(n_plots, plots, hist_name, bins_x, x_min, x_max); 
-	   	}
-
-	/**
-	 *
-	 */
+    /**
+     *  Instantiate a given number of pointers to 1D histograms of type T and
+     *  return them as a vector.  The pointers must be deleted by the user.
+     *  Note that the number of bins is calculated from the histogram range.
+     *
+     *  @param histogram_n - Number of histograms to instantiate
+     *  @param histogram_name - The name of the histogram
+     *  @param x_min - Minimum histogram value along the x-axis
+     *  @param x_min - Maximum histogram value along the x-axis
+     *  @return A vector of 1D histograms of type T
+     */
     template <typename T>
-    	void initMulti2DPlots(int n_plots, std::vector<T*> &plots, std::string hist_name,
-    							 int bins_x, int x_min, int x_max, int bins_y, int y_min, int y_max)
-    	{
-    		std::string h_name;
-    		for(int plot_n = 0; plot_n < n_plots; ++plot_n){
-    			h_name = hist_name + "_" + toString<int>(plot_n);
-    			plots.push_back(new T(h_name.c_str(), h_name.c_str(), bins_x, x_min, x_max, bins_y, y_min, y_max));
-    		}
-    	}
+    std::vector <T*> book1DHistograms(int histogram_n, std::string histogram_name,
+                                      int x_min, int x_max) {
+        int bins_x = x_max -x_min;
+        return book1DHistograms <T> (histogram_n, histogram_name,
+                                     bins_x, x_min, x_max);
+    }
 
-	/**
-	 *
-	 */
+
+    /**
+     *  Instantiate a given number of pointers to 1D histograms of type T and
+     *  return them as a vector.  The pointers must be deleted by the user.
+     *
+     *  @param histogram_n - Number of histograms to instantiate
+     *  @param histogram_name - The name of the histogram
+     *  @param bins_x - Number of bins along the x-axis
+     *  @param x_min - Minimum histogram value along the x-axis
+     *  @param x_min - Maximum histogram value along the x-axis
+     *  @return A vector of 1D histograms of type T
+     */
     template <typename T>
-        void initMulti2DPlots(int n_plots, std::vector<T*> &plots, std::string hist_name, int x_min, int x_max, int y_min, int y_max)
-        {
-            int bins_x = x_max - x_min;
-            int bins_y = y_max - y_min;
-            initMulti2DPlots(n_plots, plots, hist_name, bins_x, x_min, x_max, bins_y, y_min, y_max);
+    std::vector <T*> book1DHistograms(int histogram_n, std::string histogram_name,
+                                      int bins_x, int x_min, int x_max) {
+        std::vector <T*> histograms;
+        for (int histogram = 0; histogram < histogram_n; ++histogram) {
+            std::string name = histogram_name + ": " + toString <int> (histogram);
+            histograms.push_back(new T(name.c_str(), name.c_str(),
+                                       bins_x, x_min, x_max));
         }
+        return histograms;
+    }
+
+
+    /**
+     *  Instantiate a given number of pointers to 2D histograms of type T and
+     *  return them as a vector.  The pointers must be deleted by the user.
+     *  Note that the number of bins is calculated from the histogram range.
+     *
+     *  @param histogram_n - Number of histograms to instantiate
+     *  @param histogram_name - The name of the histogram
+     *  @param x_min - Minimum histogram value along the x-axis
+     *  @param x_min - Maximum histogram value along the x-axis
+     *  @param y_min - Minimum histogram value along the y-axis
+     *  @param y_min - Maximum histogram value along the y-axis
+     *  @return A vector of 2D histograms of type T
+     */
+    template <typename T>
+    std::vector <T*> book2DHistograms(int histogram_n, std::string histogram_name,
+    							     int x_min, int x_max,
+    							     int y_min, int y_max) {
+        int bins_x = x_max -x_min;
+        int bins_y = y_max - y_min;
+        return book2DHistograms <T> (histogram_n, histogram_name,
+                              bins_x, x_min, x_max,
+                              bins_y, y_min, y_max);
+    }
+
+    /**
+     *  Instantiate a given number of pointers to 2D histograms of type T and
+     *  return them as a vector.  The pointers must be deleted by the user.
+     *
+     *  @param histogram_n - Number of histograms to instantiate
+     *  @param histogram_name - The name of the histogram
+     *  @param bins_x - Number of bins along the x-axis
+     *  @param x_min - Minimum histogram value along the x-axis
+     *  @param x_min - Maximum histogram value along the x-axis
+     *  @param bins_y - Number of bins along the y-axis
+     *  @param y_min - Minimum histogram value along the y-axis
+     *  @param y_min - Maximum histogram value along the y-axis
+     *  @return A vector of 2D histograms of type T
+     */
+    template <typename T>
+    std::vector <T*> book2DHistograms(int histogram_n, std::string histogram_name,
+    							     int bins_x, int x_min, int x_max,
+    							     int bins_y, int y_min, int y_max) {
+        std::vector<T*> histograms;
+        for (int histogram = 0; histogram < histogram_n; ++histogram) {
+            std::string name = histogram_name + ": " + toString <int> (histogram);
+            histograms.push_back(new T(name.c_str(), name.c_str(),
+                                       bins_x, x_min, x_max,
+                                       bins_y, y_min, y_max));
+        }
+        return histograms;
+    }
 
 	/**
 	 *
