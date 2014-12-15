947a2bd760e2d9837685bceed8cbd487610bc877
Omar Moreno <omoreno1@ucsc.edu>
2014-12-15 01:24:54 -0800

    python class containing utilities used when drawing/saving ROOT plots.

---
+++ python/plotting/root_plotting_utils.py
@@ -0,0 +1,43 @@
+from fnmatch import fnmatch
+
+from ROOT import TFile
+from ROOT import TCanvas
+from ROOT import gROOT
+from ROOT import TDatime
+
+class RootPlottingUtils:
+    
+    def save_to_pdf(self, file):
+        
+        gROOT.SetBatch()
+        
+        # Check that the file is still opened.  If not, return
+        if not file.IsOpen():
+             print "[ RootPlottingUtils ]: File is not opened."
+             return
+         
+         # 
+        canvas = TCanvas("canvas", "canvas", 200, 200)
+         
+         # Iterate over all objects in the ROOT file
+        for file_key in file.GetListOfKeys():
+            print "[ RootPlottingUtils ]: Key: " + str(file_key.GetName())
+            if file_key.IsFolder():
+                file.cd(file_key.GetName())
+                output_file_name = str(file_key.GetDatime().GetDate()) + "_" + file_key.GetName() + ".pdf"
+                canvas.Print(output_file_name + "[")
+                for file_sub_key in file_key.ReadObj().GetListOfKeys():
+                    print "[ RootPlottingUtils ]: Sub key Class Name: " + str(file_sub_key.GetClassName())
+                    if fnmatch(file_sub_key.GetClassName(), "TH2*"): 
+                        file_sub_key.ReadObj().Draw("colz")
+                    elif fnmatch(file_sub_key.GetClassName(), "*Graph*"):
+                        file_sub_key.ReadObj().Draw("Ap")
+                    else:
+                        file_sub_key.ReadObj().Draw("")
+                    canvas.Print(output_file_name + "(")
+                    canvas.Clear()
+                
+                canvas.Print(output_file_name + "]")
+                file.cd()
+                
+                
