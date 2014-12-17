70e152ecd4c7c697f4619b491b7536a3d93ff3f6
Omar Moreno <omoreno1@ucsc.edu>
2014-12-16 23:56:50 -0800

    Remove debug and other clean up.

--- python/plotting/root_plotting_utils.py
+++ python/plotting/root_plotting_utils.py
@@ -1,3 +1,12 @@
+#
+#    @file root_plotting_utils.py
+#    @brief Class containing a set of ROOT plotting utilites 
+#    @author Omar Moreno <omoreno1@ucsc.edu>
+#            Santa Cruz Institute for Particle Physics
+#            University of California, Santa Cruz
+#    @date December 14, 2014
+#
+
 from fnmatch import fnmatch
 
 from ROOT import TFile
@@ -21,13 +30,12 @@ class RootPlottingUtils:
          
          # Iterate over all objects in the ROOT file
         for file_key in file.GetListOfKeys():
-            print "[ RootPlottingUtils ]: Key: " + str(file_key.GetName())
             if file_key.IsFolder():
                 file.cd(file_key.GetName())
                 output_file_name = str(file_key.GetDatime().GetDate()) + "_" + file_key.GetName() + ".pdf"
                 canvas.Print(output_file_name + "[")
                 for file_sub_key in file_key.ReadObj().GetListOfKeys():
-                    print "[ RootPlottingUtils ]: Sub key Class Name: " + str(file_sub_key.GetClassName())
+                    print "[ RootPlottingUtils ]: Saving plot " + file_sub_key.ReadObj().GetName()
                     if fnmatch(file_sub_key.GetClassName(), "TH2*"): 
                         file_sub_key.ReadObj().Draw("colz")
                     elif fnmatch(file_sub_key.GetClassName(), "*Graph*"):
