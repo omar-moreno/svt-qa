f038a8f4ff1986de43a1db073161d8486867bab1
Omar Moreno <omoreno1@ucsc.edu>
2015-02-17 07:47:15 -0800

    Add functions to get the APV25 ID (0-4) and the APV25 channel from the physical channel number.

--- src/utils/QAUtils.cxx
+++ src/utils/QAUtils.cxx
@@ -1,9 +1,15 @@
 
 #include <QAUtils.h>
 
-int QAUtils::getPhysicalChannel(int apv, int channel)
-{
+int QAUtils::getPhysicalChannel(int apv, int channel) {
 	return (QAUtils::HYBRID_CHANNELS - 1) - 
-				(apv*QAUtils::APV25_CHANNELS + (QAUtils::APV25_CHANNELS - 1) - channel); 
+			(apv*QAUtils::APV25_CHANNELS + (QAUtils::APV25_CHANNELS - 1) - channel); 
 }
-	
+
+int QAUtils::getApv25FromPhysicalChannel(int channel) { 
+    return floor(channel/128);  
+}
+
+int QAUtils::getApv25ChannelFromPhysicalChannel(int channel) { 
+    return channel%128;
+} 
