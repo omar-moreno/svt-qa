1e0447e5c75dc43f5fbe257ad82bca14f530f802
Omar Moreno <omoreno1@ucsc.edu>
2015-02-26 20:34:16 -0800

    Move the methods used to create and retrieve FebNodes from the class SvtXmlWriter to CalibrationWriter.

--- src/xml/CalibrationWriter.cxx
+++ src/xml/CalibrationWriter.cxx
@@ -12,18 +12,64 @@
 
 CalibrationWriter::CalibrationWriter() {}
 
-CalibrationWriter::~CalibrationWriter() {}
+CalibrationWriter::~CalibrationWriter() {
 
+    // Destroy all FEB nodes stored in the map
+    std::unordered_map <int, FebNode*>::iterator feb_it = feb_nodes.begin();
+    for (feb_it; feb_it != feb_nodes.end(); ++feb_it) { 
+        delete feb_it->second;
+    } 
+    feb_nodes.clear();  
+}
+
+void CalibrationWriter::open(std::string file_name) { 
+
+    // Open the XML document
+    SvtXmlWriter::open(file_name);
+
+    //  Create all of the FEB and hybrid nodes
+    for (int feb = 0; feb < 10; feb++) { 
+        this->createFebNode(feb);
+    }
+
+}
 
 void CalibrationWriter::writeBaseline(int feb_id, int hybrid_id, int channel,
-                                      int sample, double baseline) {
-//    this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
-  //          "baseline", channel, sample, baseline);
+        int sample, double baseline) {
+        
+    this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
+              "baseline", channel, sample, baseline);
 }
 
 void CalibrationWriter::writeNoise(int feb_id, int hybrid_id, int channel,
                                    int sample, double noise) {
-  //  this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
-   //         "noise", channel, sample, noise);
+    this->getFebNode(feb_id)->getHybridNode(hybrid_id)->writeChannelData(
+        "noise", channel, sample, noise);
+}
+
+void CalibrationWriter::createFebNode(int feb_id) { 
+    
+    // If a document hasn't been created, throw a runtime exception
+    if (!this->isOpen()) { 
+        throw std::runtime_error("[ ERROR ]: The XML document has not been opened."); 
+    }
+    
+    // Check if the FebNode already exists.  If it doesn't, create it.
+    if (feb_nodes.find(feb_id) == feb_nodes.end()) { 
+        feb_nodes[feb_id] = new FebNode(this->getCurrentNode(), feb_id);
+        std::cout << "[ SvtXmlWriter ]: FebNode with ID " << feb_id 
+                  << " was created." << std::endl;
+   
+    }
+}
+
+FebNode* CalibrationWriter::getFebNode(int feb_id) { 
+    
+    // If a document hasn't been created, throw a runtime exception
+    if (!this->isOpen()) { 
+        throw std::runtime_error("[ ERROR ]: The XML document has not been opened."); 
+    }
+    
+    return feb_nodes[feb_id];
 }
 
