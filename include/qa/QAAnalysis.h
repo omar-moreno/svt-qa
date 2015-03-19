59602c11d93bcb6571bfc842657dcb6247907d36
Omar Moreno <omoreno1@ucsc.edu>
2015-03-18 20:54:57 -0700

    Add a virtual method, processSamples, used to process APV25 samples, TriggerSample.  The virtual method processEvent is now used to process TriggerEvent's.

--- include/qa/QAAnalysis.h
+++ include/qa/QAAnalysis.h
@@ -10,20 +10,45 @@
 #ifndef __QA_ANALYSIS_H__
 #define __QA_ANALYSIS_H__
 
+//---------------//
 //--- SVT DAQ ---//
 //---------------//
 #include <TriggerSample.h>
+#include <TriggerEvent.h>
 
 class QAAnalysis { 
 
     public: 
 
+        /**
+         *  Destructor
+         */
         virtual ~QAAnalysis(){}; 
 
+        /**
+         *
+         */
         virtual void initialize() = 0; 
-        virtual void processEvent(TriggerSample* samples) = 0;
+        
+        /**
+         *
+         */
+        virtual void processEvent(TriggerEvent* event) = 0;
+        
+        /**
+         *
+         */
+        virtual void processSamples(TriggerSample* samples) = 0;
+
+        /**
+         *
+         */ 
         virtual void finalize() = 0; 
-		virtual std::string toString() = 0; 
+        
+        /**
+         *
+         */
+        virtual std::string toString() = 0; 
 
 }; // QAAanalysis
 
