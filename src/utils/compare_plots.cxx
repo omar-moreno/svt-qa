047d0c55d2a226aa41a2a82d49f9412f05706252
Omar Moreno <email@omarmoreno.net>
2015-09-30 21:55:00 -0700

    Get the list of files and histograms to process from a yaml config file.

--- src/utils/compare_plots.cxx
+++ src/utils/compare_plots.cxx
@@ -1,4 +1,10 @@
-
+/**
+ *  @file ComparePlots.h
+ *  @brief 
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *  @date April 29, 2015
+ *
+ */
 
 //------------------//
 //--- C++ StdLib ---//
@@ -8,106 +14,90 @@
 #include <cstdlib>
 #include <list>
 #include <fstream>
-#include <map>
 
-#include <TFile.h>
-#include <TKey.h>
-#include <TDirectory.h>
-#include <TH1.h>
-#include <TCanvas.h>
+//--------------//
+//   yaml-cpp   //
+//--------------//
+#include <yaml-cpp/yaml.h>
+
+//--------------------//
+//--- HPS Analysis ---//
+//--------------------//
+#include <ComparePlots.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
+   
     
-    string root_file_list_name;
+    string config_file_name; 
+    string style = "";
+    bool save_to_pdf = false;
 
     // Parse all the command line arguments. If there are no valid command line
     // arguments passed, print the usage and exit.
     static struct option long_options[] = {
-        {"root_list", required_argument, 0, 'l' },
+        {"config_file", required_argument, 0, 'c' }, 
+        {"style", required_argument, 0, 's'},
+        {"pdf", no_argument, 0, 'p' },
         { 0, 0, 0, 0 }
     };
 
     int option_index = 0;
     int option_char; 
-    while ((option_char = getopt_long(argc, argv, "l:", long_options, &option_index)) != -1) {
+    while ((option_char = getopt_long(argc, argv, "c:s:p", long_options, &option_index)) != -1) {
         switch (option_char) {
-            case 'l': 
-                root_file_list_name = optarg;
+            case 'c': 
+                config_file_name = optarg; 
+                break;
+            case 's':
+                style = optarg; 
+                break;
+            case 'p':
+                save_to_pdf = true;
+                break;
+            default: 
                 break;
         }
     }
 
-    if (root_file_list_name.empty()) { 
-        cerr << "Please specify a list of ROOT files to process." << endl;
-        return EXIT_FAILURE;
+    // Require a configuration
+    if (config_file_name.empty()) { 
+        cerr << "Please specify a configuration file." << endl;
+        return EXIT_FAILURE; 
     }
 
-    // Create the list of files to process
-    //list<string> root_files; 
-    list< TFile* > root_files;
-    string root_file; 
-    ifstream root_file_list(root_file_list_name.c_str(), ifstream::in);
-    if (!root_file_list.is_open()) { 
-        cerr << "Failed to open file " << root_file_list_name << endl;
+    // TODO: This all needs to move to it's own config parse method
+    YAML::Node config = YAML::LoadFile(config_file_name);
+    if (config.IsNull()) { 
+        cerr << "Unable to open configuration file " << config_file_name << std::endl;
         return EXIT_FAILURE;
-    }
-
-    while (root_file_list >> root_file) { 
-        root_files.push_back(new TFile(root_file.c_str())); 
-        if (root_files.back()->IsZombie()) { 
-            cout << "Failed to open root file " << root_file << endl;
-            return EXIT_FAILURE;
-        }
     } 
-    root_file_list.close();
-
-    map <string, vector<TH1*>> histogram_map; 
-
-    // Loop over all of the ROOT files and create the plot maps
-    //list<string>::iterator root_file_it = root_files.begin();
-    list < TFile* >::iterator root_file_it = root_files.begin();
-    for (root_file_it; root_file_it != root_files.end(); ++root_file_it) { 
-        cout << "[ ]: Processing file: " << (*root_file_it)->GetName() << endl;
-            
-        TIter next((*root_file_it)->GetListOfKeys());
-        while (TKey *key = (TKey*) next()) { 
-            cout << "[ ]: Key: " << key->GetName() << endl;
-        
-            if (key->IsFolder()) { 
-                (*root_file_it)->cd(key->GetName());
-                TIter next_sub(((TDirectory*) key->ReadObj())->GetListOfKeys());
-                while (TKey *sub_key = (TKey*) next_sub()) { 
-                    cout << "[ ]: Sub key: " << sub_key->GetName() << endl;
-                        histogram_map[sub_key->GetName()].push_back(((TH1*) sub_key->ReadObj()));
-                    cout << "Map size: " << histogram_map.size() << endl;
-                    cout << "Vector size: " << histogram_map[sub_key->GetName()].size() << endl;
-                }
-            }
-        }
+    
+    YAML::Node root_file_names = config["Files"];
+    if (root_file_names.IsNull()) { 
+        cerr << "Please specify a list of ROOT files to process." << endl;
+        return EXIT_FAILURE;    
     }
-       
-    TCanvas* canvas = new TCanvas("canvas", "canvas", 500, 500); 
-    map <string, vector<TH1*>>::iterator histogram_it = histogram_map.begin();
-    canvas->Print("test.pdf[");
-    for (histogram_it; histogram_it != histogram_map.end(); histogram_it++) { 
-        int color_index = 1;
-        histogram_it->second[0]->SetLineColor(color_index);
-	histogram_it->second[0]->GetYaxis()->SetRangeUser(0, 500);
-        ++color_index;
-	histogram_it->second[0]->Draw();
 
-        for (int hist_n = 1; hist_n < histogram_it->second.size(); hist_n++) { 
-           histogram_it->second[hist_n]->SetLineColor(color_index);
-	   ++color_index;
-	   histogram_it->second[hist_n]->GetYaxis()->SetRangeUser(0, 500);
-           histogram_it->second[hist_n]->Draw("same");  
-            cout << "Draw : " << hist_n << endl;
-       } 
-        canvas->Print("test.pdf(");
+    list< TFile* > root_files;
+    for (auto root_file_name : root_file_names) { 
+        root_files.push_back(new TFile(root_file_name.as<string>().c_str())); 
+        if (root_files.back()->IsZombie()) { 
+            cout << "Failed to open ROOT file " << root_file_name << std::endl;
+            return EXIT_FAILURE; 
+        } 
     }
-    canvas->Print("test.pdf]");
+
+    ComparePlots* comparator = new ComparePlots();
+    if (!style.empty()) comparator->setStyle(style);
+
+    YAML::Node histogram_names = config["Histograms"];
+    if (!histogram_names.IsNull()) { 
+        comparator->parseFiles(root_files, histogram_names[0].as<string>().c_str());
+    } else comparator->parseFiles(root_files);
+    comparator->overlayPlots(); 
+
     return EXIT_SUCCESS;
 }
 
