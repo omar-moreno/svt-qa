a1cd8cb1e1b210284d66b12e1f01ce631b7bbba9
omar-moreno <email@omarmoreno.net>
2018-10-22 12:49:27 -0700

    Class encapsulating utilities used by the QA.

---
+++ src/utils/QAUtils.cxx
@@ -0,0 +1,20 @@
+/**
+ * @file QAUtils.cxx
+ * @brief Class encapsulating utilities used by the QA
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#include <QAUtils.h>
+
+int QAUtils::getPhysicalChannel(int apv, int channel) {
+	return (QAUtils::HYBRID_CHANNELS - 1) - 
+			(apv*QAUtils::APV25_CHANNELS + (QAUtils::APV25_CHANNELS - 1) - channel); 
+}
+
+int QAUtils::getApv25FromPhysicalChannel(int channel) { 
+    return 4 - floor(channel/128);  
+}
+
+int QAUtils::getApv25ChannelFromPhysicalChannel(int channel) { 
+    return channel%128;
+} 
