7e12b142a967ad2620d8cd2a4628d5d516afbce5
omar-moreno <email@omarmoreno.net>
2019-03-26 22:08:38 -0700

    Update temperature calculation.

--- arduino/htm2500lf/htm2500lf.ino
+++ arduino/htm2500lf/htm2500lf.ino
@@ -8,15 +8,20 @@
  * When using an Arduino Leonardo, the analog reading is digitized 
  * using a 10 bit ADC. The value is converted back to a voltage before
  * the humidity value is calculated.  This allows reading the humidity
- * to within ~2%. 
+ * to within ~2% between 10% and 95%.  Below and above those regions,  
+ * the accuracy becomes ~10%.
  * 
  * The humidity is calculated using the equation
  * 
- * RH = 0.0375*V_{out} - 37.7
+ * RH = -1.92e-9*V_{out}^3 + 1.44e-5*V_{out}^2 + 3.4e-3*V_{out} - 12.4
+ * 
+ * where V_{out} is in mV.
  * 
  * Omar Moreno, SLAC National Accelerator Laboratory
  * 
  */
+
+#include <math.h>
 
 // The setup routine runs once when reset is pressed
 void setup() {
@@ -33,13 +38,27 @@ void loop() {
   int sensorValue = analogRead(A0);
 
   // Convert the analog reading (which goes from 0-1023) to
-  // a voltage (0-5V)
-  float voltage = sensorValue * (5.0/1023.0);
+  // a voltage (0-5000 mV)
+  float voltage = sensorValue * (5.0/1023.0)*1000;
 
-  // Calculate the relative humidity
-  float RH = 0.0375*voltage - 37.7; 
+  // Calculate the Relative Humidity
+  float RH = -1.92e-9*pow(voltage,3) + 1.44e-5*pow(voltage,2) + 3.4e-3*voltage - 12.4;
 
   // Print the value
   Serial.println(RH);
+
+  /*
+  sensorValue = analogRead(A1);
+  float NTC = sensorValue * (5.0/1023.0);
+  float a = 8.54942e-4;
+  float b = 2.57305e-4;
+  float c = 1.65368e-7;
+  float T = 1/(a + b*log(NTC) + c*log(NTC)*log(NTC)*log(NTC));
+  T = T - 273.15;
+
+  Serial.println(NTC);
+  Serial.println(T);
+  */
+   
 
 }
