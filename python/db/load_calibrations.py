4e8c75a0ec07ee1aeba11a99b996492a65d8334c
Omar Moreno <email@omarmoreno.net>
2019-07-23 16:17:28 -0400

    Update calibration loading.  Automatically add a conditions record for each set of calibrations loaded.

--- python/db/load_calibrations.py
+++ python/db/load_calibrations.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 import argparse
@@ -10,6 +9,8 @@ def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-f', action='store', dest='cfile', 
                         help='Calibrations file to load into the database.')
+    parser.add_argument('-r', action='store', dest='run', 
+                        help='Run number.')
     args = parser.parse_args()
 
     host = 'hpsdb.jlab.org'
@@ -27,44 +28,51 @@ def main():
     # Get a cursor
     cursor = db.cursor()
 
-    #
+    sql_query = "SELECT MAX(collection_id) FROM svt_calibrations"
+    try:
+        cursor.execute(sql_query)
+        results = cursor.fetchall()
+    except:
+        print "[ERROR]: Unable to load pedestal and noise values."
+        db.rollback()
+
+    max_col_id = results[0][0]
+    col_id = max_col_id + 1
+
+    print 'Loading SVT calibrations with collection ID %s' % col_id
+
     cfile = open(args.cfile)
-    
     for line in cfile: 
-        print line
-
-    '''
-    if args.cal_col_id: 
-        # Check if the collection id exist.  If so, warn the user and exit.
-        col_id = int(args.cal_col_id.strip()) 
-        sql_query = "SELECT * FROM svt_calibrations WHERE collection_id = '%i'" % col_id 
-    
-        try: 
+        values = [float(x) for x in line.split(" ")]
+        try:
+            sql_query = "INSERT INTO svt_calibrations(collection_id, svt_channel_id,\
+                pedestal_0, pedestal_1, pedestal_2, pedestal_3, pedestal_4, pedestal_5,\
+                noise_0, noise_1, noise_2, noise_3, noise_4, noise_5)\
+                VALUES('%i', '%i', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f')"\
+                % (col_id, values[0],\
+                    values[1], values[2], values[3], values[4], values[5], values[6],\
+                    values[7], values[8], values[9], values[10], values[11], values[12])
+            #print sql_query    
             cursor.execute(sql_query)
-            results = cursor.fetchall()
-            
-            if len(results) != 0: 
-                print 'Collection exists!'
-                return 
-
-            print "Loading default values of pedestal and noise for all SVT channels ..."
-            pedestal = 6000.
-            noise = 65
-            for svt_channel_id in range(0, 24576):
-                sql_query = "INSERT INTO svt_calibrations(collection_id, svt_channel_id,\
-                    pedestal_0, pedestal_1, pedestal_2, pedestal_3, pedestal_4, pedestal_5,\
-                    noise_0, noise_1, noise_2, noise_3, noise_4, noise_5)\
-                    VALUES('%i', '%i', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f')"\
-                    % (col_id, svt_channel_id,\
-                    pedestal, pedestal, pedestal, pedestal, pedestal, pedestal,\
-                    noise, noise, noise, noise, noise, noise)
-
-                cursor.execute(sql_query)
-            db.commit()
         except:
             print "[ERROR]: Unable to load pedestal and noise values."
             db.rollback()
-    '''
+    db.commit()
+    
+    print 'Adding conditions record.'  
+ 
+    sql_query = "INSERT INTO conditions(run_start, run_end, updated, created, \
+        tag, created_by, notes, name, table_name, collection_id) \
+        value ('%i', 999999, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'phys2019', 'omoreno', \
+        'Pedestals and noise extracted from run %s.', 'svt_calibrations', 'svt_calibrations', '%i')"\
+        % (int(args.run.strip()), int(args.run.strip()), col_id)
 
+    try: 
+        cursor.exectute(sql_query)
+    except: 
+        print "[ ERROR ]: Unable to load conditions record."
+        db.rollback()
+    db.commit()
+ 
 if __name__ == "__main__": 
     main()
