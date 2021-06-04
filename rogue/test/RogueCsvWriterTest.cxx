a1f825f9086af9846a5d21f17fea823d0d36df5c
Omar Moreno <email@omarmoreno.net>
2021-06-04 16:12:48 -0700

    Update the test.

--- rogue/test/RogueCsvWriterTest.cxx
+++ rogue/test/RogueCsvWriterTest.cxx
@@ -2,14 +2,15 @@
 #include <iostream>
 
 //---< Rogue >---//
+#include "rogue/Helpers.h"
 #include "rogue/utilities/fileio/StreamReader.h"
 
 //---< rogue >---//
-#include "rogue/RogueDataReceiver.h"
+#include "rogue/RogueCsvWriter.h"
 
 namespace rogue {
 
-TEST_CASE("Rogue reader", "[rogue]") {
+TEST_CASE("CSV Writer", "[rogue]") {
 
   // Instantiate the reader used to stream frames from a file created
   // by Rogue
@@ -17,7 +18,16 @@ TEST_CASE("Rogue reader", "[rogue]") {
 	  = std::make_shared<rogue::utilities::fileio::StreamReader>();
 
   // Instantiate the receiver used to process the frames
-  std::shared_ptr<RogueDataReceiver> receiver 
-	  = std::make_shared<RogueDataReceiver>();
+  std::shared_ptr<RogueCsvWriter> receiver 
+  	  = std::make_shared<RogueCsvWriter>();
+  
+  // Now connect the two streams
+  rogueStreamConnect(reader, receiver);
+
+  // Open the file for reading
+  reader->open("/home/omoreno/projects/hps/data/febs/data_20210406_145159.dat");  
+
+  // Close the streams
+  reader->closeWait(); 
 }
 }
