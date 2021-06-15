32d0edb66716e78246500a4c73ebc3b22225124e
Omar Moreno <email@omarmoreno.net>
2021-06-15 14:22:05 -0700

    Dump the sample block to a file.

--- rogue/src/rogue/RogueCsvWriter.cxx
+++ rogue/src/rogue/RogueCsvWriter.cxx
@@ -37,7 +37,29 @@ void RogueCsvWriter::acceptFrame(
   std::vector<SampleBlock> blocks = parser->parse(frame);
 
   // If there aren't any sample blocks, skip the rest of processing
-  if (blocks.empty()) return; 
+  if (blocks.empty())
+    return;
 
+  // Write out the blocks to csv
+  for (auto &block : blocks) {
+    int pchannel =
+        (5 * 128 - 1) - (block.apv * 128 + (128 - 1) - block.channel);
+    csv << block.rce << "," << block.feb << "," << block.hybrid << ","
+        << block.apv << "," << block.channel << "," << pchannel << ","
+        << block.samples[0] << "," << block.samples[1] << ","
+        << block.samples[2] << "," << block.samples[3] << ","
+        << block.samples[4] << "," << block.samples[5] << "," << block.error
+        << "," << block.head << "," << block.tail << "," << block.filter
+        << std::endl;
+  }
 }
+
+void RogueCsvWriter::open(const std::string &file_path) {
+  csv.open(file_path);
+  csv << "RCE,FEB,Hybrid,APV,channel,pchannel,sample0,sample1,sample2,"
+         "sample3,sample4,sample5,error,head,tail,filter"
+      << std::endl;
+}
+
+void RogueCsvWriter::close() { csv.close(); }
 } // namespace rogue
